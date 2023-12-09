bb8f2ecfc9cb13e252cf1542512a76fcfdb07520
N0manz <136736358+N0manz@users.noreply.github.com>
2023-12-09 13:25:20 +0300

    Add files via upload
    new shuffle

--- shufle_data.py
+++ shufle_data.py
@@ -1,33 +1,14 @@
-import openfoamparser_mai as Ofpp
+
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-points=Ofpp.parse_internal_field(r'data\0.3M\150\C')
-p = Ofpp.parse_internal_field(r'data\0.3M\150\p')
-u = Ofpp.parse_internal_field(r'data\0.3M\150\U')
-
-x = []
-y = []
-z = []
-for point in points:
-    x.append(point[0])
-    y.append(point[1])
-    z.append(point[2])
+def train_test(name):
+    df = pd.read_csv(f'{name}.csv')
+    df = shuffle(df)
+    train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+    features = df[['x', 'y', 'z', 'v_x', 'v_y', 'v_z']].values
+    target = df['pressure'].values
+    train_x, test_x, train_Y, test_Y = train_test_split(features, target, test_size=0.2, random_state=42)
     
-v_x = []
-v_y = []
-v_z = []
-for v in u:
-    v_x.append(v[0])
-    v_y.append(v[1])
-    v_z.append(v[2])
-    
-df = pd.DataFrame({'x': x, 'y': y, 'z': z, 'v_x': v_x, 'v_y': v_y, 'v_z': v_z, 'pressure': p})
-df = shuffle(df)
-
-def train_val_test(df):
-    train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
-    val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
-
-    return train_df, val_df, test_df
+    return train_x, train_Y, test_x, test_Y
